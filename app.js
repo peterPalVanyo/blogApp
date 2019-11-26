a96d9385c01238e450a44a4db831fc684dc9f2ca
peterPalVanyo <vanyo.peter.pal@gmail.com>
2019-11-26 05:48:00 +0100

    model schema

--- app.js
+++ app.js
@@ -3,10 +3,25 @@ const bodyParser = require("body-parser"),
   mongoose = require("mongoose"),
   express = require("express"),
   app = express();
+//app config
+mongoose.set("useUnifiedTopology", true);
+mongoose.connect("mongodb://localhost:27017/blogApp", {
+  useNewUrlParser: true
+});
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+//model
+const blogSchema = mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+})
+const Blog = mongoose.model('Blog', blogSchema)
+//routes
 
 
-
-
-  app.listen(3000, () => {
-      console.log('the server is running')
-  })
+app.listen(3000, () => {
+  console.log("the server is running");
+});
