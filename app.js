5ac87e4e2dc8deec1c810e4f0c028cbc678e79c6
peterPalVanyo <vanyo.peter.pal@gmail.com>
2019-11-26 05:15:48 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+//get data out the form, form send to req body and we parse it
+const bodyParser = require("body-parser"),
+  mongoose = require("mongoose"),
+  express = require("express"),
+  app = express();
+
+
+
+
+  app.listen(3000, () => {
+      console.log('the server is running')
+  })
